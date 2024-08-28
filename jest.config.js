43301084d24f09442faa4f0af99e558c161be40d
Kevin Beliën <60871283+KevinBelien@users.noreply.github.com>
2024-08-29 00:10:52 +0200

    Feat/jest (#2)
    * feat: try jest
    
    * feat: jest
    
    ---------
    
    Co-authored-by: Kevin Beliën <kevin.belien@trinovation.be>

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globals: {
+    'ts-jest': {
+      stringifyContentPathRegex: '\\.(html|svg)$',
+
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+   moduleNameMapper: {
+    '^@chit-chat/ng-chat/src/lib/(.*)$': '<rootDir>/projects/chit-chat/src/lib/$1',
+    // add other mappings if necessary
+  },
+
+};
